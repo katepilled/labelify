86b25eb186d3938cb77e1e4cc61b8f82b5b55396
katepilled <katehyoon@gmail.com>
2023-05-12 23:54:15 -0400

    install react

---
+++ labelify/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./pages/home";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div>
+    <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ labelify/src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+    return(
+    <div> Labelify </div>
+    );
+}
+
+export default Header;
